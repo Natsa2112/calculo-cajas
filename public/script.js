b5f7e18a4bc88f5c0dd6d9b6eec1c9f210ef1aec
Claudio Natsa <claudio.natsa@gmail.com>
2024-02-06 02:52:03 -0300

    Se agrego el boton RESET (Limpiar)

--- public/script.js
+++ public/script.js
@@ -3,7 +3,7 @@ function calculateBoxes() {
   var unitsPerBox = parseInt(document.getElementById('unitsPerBox').value);
   
   if (isNaN(units) || isNaN(unitsPerBox) || units <= 0 || unitsPerBox <= 0) {
-    alert('Por favor, ingrese números válidos mayores que cero.');
+    alert('Por favor, ingrese números válidos mayores que cero. HDP!!!');
     return;
   }
   
@@ -15,13 +15,13 @@ function calculateBoxes() {
   if (boxes > 0) {
     if (boxes === 1) {
       if (remainder > 0) {
-        result.textContent = `Es ${boxes} caja con ${unitsPerBox} unidades y resto una caja con ${remainder} unidades.`;
+        result.textContent = `Es ${boxes} caja con ${unitsPerBox} unidades y una caja con ${remainder} unidades.`;
       } else {
         result.textContent = `Es ${boxes} caja con ${unitsPerBox} unidades.`;
       }
     } else {
       if (remainder > 0) {
-        result.textContent = `Son ${boxes} cajas con ${unitsPerBox} unidades y resto una caja con ${remainder} unidades.`;
+        result.textContent = `Son ${boxes} cajas con ${unitsPerBox} unidades y una caja con ${remainder} unidades.`;
       } else {
         result.textContent = `Son ${boxes} cajas con ${unitsPerBox} unidades.`;
       }
@@ -29,4 +29,10 @@ function calculateBoxes() {
   } else {
     result.textContent = `Una caja con ${units} unidades.`;
   }
+}
+
+function resetFields() {
+  document.getElementById('units').value = '';
+  document.getElementById('unitsPerBox').value = '';
+  document.getElementById('result').textContent = '';
 }
